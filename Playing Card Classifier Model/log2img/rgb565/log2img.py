925feb94dbbd8d64d2506dbfe7cbaec6ef92bfa0
Vincentbnguyen <90225857+Vincentbnguyen@users.noreply.github.com>
2024-04-14 02:40:34 -0500

    add new 128 x 128 ml model

--- Playing Card Classifier Model/log2img/rgb565/log2img.py
+++ Playing Card Classifier Model/log2img/rgb565/log2img.py
@@ -4,7 +4,8 @@ import os
 
 pixel_data = []
 found_start = False
-with open('log_data/CT.log', 'r', encoding='utf-8', errors='ignore') as file:
+
+with open('Playing Card Classifier Model/log_data/C2.log', 'r', encoding='utf-8', errors='ignore') as file:
     for line in file:
         # skip straight to the data
         if not found_start:
@@ -14,12 +15,10 @@ with open('log_data/CT.log', 'r', encoding='utf-8', errors='ignore') as file:
         else:
             parts = line.split(',')
             for part in parts:
-                print(part)
                 if 'data:' in part:
                     # Extract the hex after 'data:'
                     word = part.split(': ')[1].strip().zfill(8)
                     # word = [R1, G1, B1, R0, G0, B0]
-                    print(f"word: {word}")
                     int_val = int(word, 16)  # Convert hexadecimal string to integer
 
                     # Extract individual R, G, and B components
@@ -38,10 +37,26 @@ with open('log_data/CT.log', 'r', encoding='utf-8', errors='ignore') as file:
 
 height = 148
 width = 172
-data = np.array(pixel_data)
-target_shape = (height, width, 3)
 
-image_data = np.zeros((height * width, 3), dtype=np.uint8)
+# reshape into 128 x 128
+new_pixel_data = [0] * 128 * 128
+ml_height = 128
+ml_width = 128
+
+factor_x = width / ml_width
+factor_y = height / ml_height
+
+for i in range(128):
+    for j in range (128):
+        col = int(j * factor_x)
+        row = int(i * factor_y)
+        val = pixel_data[row * width + col]
+        new_pixel_data[i * 128 + j] = val
+
+data = np.array(new_pixel_data)
+target_shape = (ml_height, ml_width, 3)
+
+image_data = np.zeros((ml_height * ml_width, 3), dtype=np.uint8)
 image_data[:len(data)] = data
 image_data = image_data.reshape(target_shape)
 image = Image.fromarray(image_data)
