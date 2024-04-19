906688f76dce1dbb55ccbf12a2e0ceaee52f09ec
Vincentbnguyen <90225857+Vincentbnguyen@users.noreply.github.com>
2024-04-19 17:19:04 -0500

    128 x 128 RGB model

--- Playing Card Classifier Model/log2img/rgb565/log2img.py
+++ Playing Card Classifier Model/log2img/rgb565/log2img.py
@@ -5,7 +5,7 @@ import os
 pixel_data = []
 found_start = False
 
-with open('Playing Card Classifier Model/log_data/C2.log', 'r', encoding='utf-8', errors='ignore') as file:
+with open('Playing Card Classifier Model/logdata/H9.log', 'r', encoding='utf-8', errors='ignore') as file:
     for line in file:
         # skip straight to the data
         if not found_start:
@@ -13,6 +13,8 @@ with open('Playing Card Classifier Model/log_data/C2.log', 'r', encoding='utf-8'
                 found_start = True
             continue
         else:
+            if "AI" in line:
+                break
             parts = line.split(',')
             for part in parts:
                 if 'data:' in part:
@@ -29,34 +31,20 @@ with open('Playing Card Classifier Model/log_data/C2.log', 'r', encoding='utf-8'
                     G1 = ((int_val >> 5) & 0b111111) << 2
                     B1 = (int_val & 0b11111) << 3
 
-                    p0 = [R0, G0, B0]
-                    p1 = [R1, G1, B1]
+                    # NOTE: reorder these RGB values to see which best fits the ml model and camera output
+                    p0 = [G0, B0, R0]
+                    p1 = [G1, B1, R1]
                     
                     pixel_data.append(p1)
                     pixel_data.append(p0)
 
-height = 148
-width = 172
+height = 128
+width = 128
 
-# reshape into 128 x 128
-new_pixel_data = [0] * 128 * 128
-ml_height = 128
-ml_width = 128
+data = np.array(pixel_data)
+target_shape = (height, width, 3)
 
-factor_x = width / ml_width
-factor_y = height / ml_height
-
-for i in range(128):
-    for j in range (128):
-        col = int(j * factor_x)
-        row = int(i * factor_y)
-        val = pixel_data[row * width + col]
-        new_pixel_data[i * 128 + j] = val
-
-data = np.array(new_pixel_data)
-target_shape = (ml_height, ml_width, 3)
-
-image_data = np.zeros((ml_height * ml_width, 3), dtype=np.uint8)
+image_data = np.zeros((height * width, 3), dtype=np.uint8)
 image_data[:len(data)] = data
 image_data = image_data.reshape(target_shape)
 image = Image.fromarray(image_data)
